524fdce8dbc14e90586b6c01724d2e28c3e57514
biniam <biniam@ca.ibm.com>
2018-02-02 12:42:54 -0500

    Drop Node.js 6 support
    LoopBack4 is dropping support for Node 6 because
    it is no longer LTS and doesn't support newer
    features. It's best for extensions to align with
    it. Also added a dummy unit test, and updated
    dependencies.
    
    BREAKING CHANGE: Support for Node.js version lower
    than 8.0 has been dropped. Please upgrade to Node
    8 or higher.

--- index.js
+++ index.js
@@ -3,7 +3,4 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-const nodeMajorVersion = +process.versions.node.split('.')[0];
-module.exports = nodeMajorVersion >= 7 ?
-  require('./dist/src') :
-  require('./dist6/src');
+module.exports = require('./dist');
